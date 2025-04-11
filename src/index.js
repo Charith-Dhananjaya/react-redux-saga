069d76b7814c38e8f648a5531c5c1f2a8c996024
Charith-Dhananjaya <114970084+Charith-Dhananjaya@users.noreply.github.com>
2025-04-11 16:29:28 +0530

    add button

--- src/index.js
+++ src/index.js
@@ -2,16 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {provider} from 'react-redux';
+import {Provider} from 'react-redux';
 import {configureStore} from '@reduxjs/toolkit';
+import counterReducer from './counterState';
 
 
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer
+  }
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 
